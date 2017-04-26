74108db30c66397b24f909a074d8b75dd2fa0200
Peter McCloskey <pcm17@pitt.edu>
2017-04-26 11:40:11 -0400

    rename diabetes

--- Machine Learning/Diabetes Diagnosing/run_kNN.m
+++ Machine Learning/Diabetes Diagnosing/run_kNN.m
@@ -1,6 +1,5 @@
 %%% kNN classifier for pima
 %%% *************************************************************
-%%% Milos Hauskrecht
 %%% CS1675 Intro to Machine Learning, University of Pittsburgh
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -15,16 +14,27 @@ n_features = data_col - 1
 
 %%% create x
 x = tr_data(:,1:n_features);
+x = x/norm(x);
 %% create y vector
 y=tr_data(:,data_col);
  
 %% builds x for the the test set
 x_test = test_data(:,1:n_features);
+x_test = normalize(x_test);
+myxtest = (x_test - min(x_test))./(max(x_test)-min(x_test)); 
 %% builds y vector for the test set
 y_test=test_data(:,data_col);
 
 %%%% classify examples in the test set using the 3NN classifier using the
 %%%% Euclidean metric
-y_pred=knnclassify(x_test,x,y,3,'euclidean')
-error= sum(abs(y_pred-y_test))/size(y_test,1)
-accuracy=1-error
+neighbors = linspace(1,5,5);
+N = numel(neighbors);
+for n =1:N
+    y_pred=knnclassify(x_test,x,y,neighbors(n),'euclidean');
+    error= sum(abs(y_pred-y_test))/size(y_test,1);
+    accuracy(n)=1-error;
+end
+figure,plot(neighbors, accuracy);
+title('Data Normalized');
+xlabel('Number of Neighbors');
+ylabel('Classification Accuracy');
